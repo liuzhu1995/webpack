46e8820769eec52e54c9984766d1c0f24d060e35
liuzhu <1014166283@qq.com>
2019-10-23 18:58:00 +0800

    Use npm scripts

--- HelloWorld/dist/bundle.js
+++ HelloWorld/dist/bundle.js
@@ -81,32 +81,32 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./index.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ "./add-content.js":
-/*!************************!*\
-  !*** ./add-content.js ***!
-  \************************/
+/***/ "./src/add-content.js":
+/*!****************************!*\
+  !*** ./src/add-content.js ***!
+  \****************************/
 /*! exports provided: default */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function() {\r\n  document.write('Hello World!');\r\n});\r\n\n\n//# sourceURL=webpack:///./add-content.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (function() {\r\n  document.write('Hello World!');\r\n});\r\n\n\n//# sourceURL=webpack:///./src/add-content.js?");
 
 /***/ }),
 
-/***/ "./index.js":
-/*!******************!*\
-  !*** ./index.js ***!
-  \******************/
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
 /*! no exports provided */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _add_content__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./add-content */ \"./add-content.js\");\n\r\ndocument.write('My first Webpack app.</br>');\r\n\r\nObject(_add_content__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _add_content__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./add-content */ \"./src/add-content.js\");\n\r\ndocument.write('My first Webpack app.</br>');\r\n\r\nObject(_add_content__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\r\n\n\n//# sourceURL=webpack:///./src/index.js?");
 
 /***/ })
 
