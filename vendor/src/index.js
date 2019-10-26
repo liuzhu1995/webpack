621c51aff99f93c8a04584b54e74d51abc12f6b3
liuzhu <1014166283@qq.com>
2019-10-26 15:17:10 +0800

    Use vendor

---
+++ vendor/src/index.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import addContent from './write';
+
+$('#root').append('What happened to you?</br>');
+addContent();
+
---
+++ vendor/webpack.config.js
@@ -0,0 +1,10 @@
+const config = {
+  entry: {
+    index: "./src/index.js",
+    vendor: ["jquery"]
+  },
+  mode: "development",
+
+};
+
+module.exports = config;
